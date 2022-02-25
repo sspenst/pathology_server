5a18544d60f120cb9a304e37677765e0363a11c9
Spencer Spenst <spencerspenst@gmail.com>
2022-02-24 21:09:04 -0800

    try specifying methods

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const port = process.env.PORT || 8000;
 app.use(cookieParser());
 app.use(cors({
   credentials: true,
+  methods: ['GET', 'PUT', 'POST'],
   origin: process.env.ORIGIN,
 }));
 app.use(express.json());
