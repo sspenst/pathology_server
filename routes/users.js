fc20abdc241c2d970fd22686b9c488d02a0b98e1
Spencer Spenst <spencerspenst@gmail.com>
2022-02-24 21:19:11 -0800

    need https if samesite is none

--- routes/users.js
+++ routes/users.js
@@ -10,7 +10,7 @@ function addTokenCookie(res, email) {
   const token = jwt.sign(payload, process.env.SECRET, {
     expiresIn: '1d'
   });
-  res.cookie('token', token, { httpOnly: true, sameSite: 'none' });
+  res.cookie('token', token, { httpOnly: true, sameSite: 'none', secure: true });
 }
 
 router.post('/signup', function(req, res) {
